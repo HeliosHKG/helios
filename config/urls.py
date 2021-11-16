0d6a716c73b02cda19edff49218d843f035d0d73
Gilijar <64728689+Gilijar@users.noreply.github.com>
2021-11-16 09:59:27 +0100

    Update Frontend

--- config/urls.py
+++ config/urls.py
@@ -17,6 +17,9 @@ urlpatterns = [
     path("users/", include("helios.users.urls", namespace="users")),
     path("accounts/", include("allauth.urls")),
     # Your stuff: custom urls includes go here
+    
+    
+    
 ] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
 if settings.DEBUG:
     # Static file serving when using Gunicorn + Uvicorn for local web socket development
--- helios/vorstudie/apps.py
+++ helios/vorstudie/apps.py
@@ -2,4 +2,4 @@ from django.apps import AppConfig
 
 
 class VorstudieConfig(AppConfig):
-    name = 'vorstudie'
+    name = 'helios.vorstudie'
