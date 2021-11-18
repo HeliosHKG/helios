116627b10e53c9a5e897578bdd5e5da899df1b85
jouni <jouniw@gmx.ch>
2021-11-18 11:38:26 +0100

    stammdaten model

--- helios/vorstudie/admin.py
+++ helios/vorstudie/admin.py
@@ -2,8 +2,10 @@ from django.contrib import admin
 
 
 
-from .models import Input_Investitionskosten, Technikflaechen, Leistung
+from .models import Energie, Investitionskosten, Nutzungskosten, Technikflaechen, Leistung
 
 admin.site.register(Technikflaechen)
 admin.site.register(Leistung)
-admin.site.register(Input_Investitionskosten)
+admin.site.register(Investitionskosten)
+admin.site.register(Energie)
+admin.site.register(Nutzungskosten)
--- helios/vorstudie/models.py
+++ helios/vorstudie/models.py
@@ -3,67 +3,15 @@ from django.db import models
 from django.db.models.deletion import CASCADE, SET, SET_NULL
 from django.db.models.fields import CharField, FloatField, IntegerField
 from django.db.models.fields.related import ForeignKey
+from helios import projekt
 
-from helios.projekt.models import Abgabesystem, Erzeugungstyp, Gebaudenutzung, Gewerk, Gewerk2, Klassifizierung, Kostenstammdaten_Elektro, Kostenstammdaten_HLKS_Abgabe, Nutzungsstammdaten_SIA2024, Projekt, ProjektSpezifikationen, Raumnutzung
-
-
-class Input_Investitionskosten(models.Model):
-    projekt = ForeignKey(Projekt, on_delete=CASCADE, null=True, blank=True)
-    flaeche = IntegerField()
-    gebaudenutzung = ForeignKey(Gebaudenutzung, on_delete=SET_NULL, null=True)
-    raumnutzung = ForeignKey(Raumnutzung, on_delete=SET_NULL, null=True)
-    gewerk = ForeignKey(Gewerk, on_delete=SET_NULL, null=True)
-    abgabesystem = ForeignKey(Abgabesystem, on_delete=SET_NULL, null=True)
-    einheitspreis_pro_m2_elektro = IntegerField()
-    einheitspreis_pro_m2_hlks_abgabe = IntegerField()
-    einheitspreis_pro_m2_hlks_erzeugung = IntegerField()
-
-    def __str__(self):
-        return self.projekt or ''
-
-    def __str__(self):
-        return self
-
-    def __init__(self):
-        self.einheitspreis_pro_m2_hlks_abgabe = self.getSanitaerKostenAbgabe + \
-            self.getHeizungsKostenAbgabe + self.getLueftungsKostenAbgabe + self.getKaeltKostenAbgabe
-        return self
-
-    def getElektroKostenAbgabe(self):
-        kostenstammdaten_Elektro: Kostenstammdaten_Elektro = Kostenstammdaten_Elektro.objects.get(
-            gewerk=self.gewerk, raumnutzung=self.raumnutzung, gebaudenutzung=self.gebaudenutzung)
-        pro_m2 = kostenstammdaten_Elektro.einheitspreis_pro_m2
-        return pro_m2 * self.flaeche
-
-    def getSanitaerKostenAbgabe(self):
-        kostenstammdaten_HLKS_Abgabe: Kostenstammdaten_HLKS_Abgabe = Kostenstammdaten_HLKS_Abgabe.objects.get(
-            gewerk=self.gewerk, raumnutzung=self.raumnutzung, abgabesystem=self.abgabesystem, gebaudenutzung=self.gebaudenutzung)
-        pro_m2 = kostenstammdaten_HLKS_Abgabe.einheitspreis_pro_m2
-        return pro_m2 * self.flaeche
-
-    def getHeizungsKostenAbgabe(self):
-        kostenstammdaten_HLKS_Abgabe: Kostenstammdaten_HLKS_Abgabe = Kostenstammdaten_HLKS_Abgabe.objects.get(
-            gewerk=self.gewerk, raumnutzung=self.raumnutzung, abgabesystem=self.abgabesystem, gebaudenutzung=self.gebaudenutzung)
-        pro_kw = kostenstammdaten_HLKS_Abgabe.einheitspreis_pro_m2
-        return pro_kw
-
-    def getLueftungsKostenAbgabe(self):
-        kostenstammdaten_HLKS_Abgabe: Kostenstammdaten_HLKS_Abgabe = Kostenstammdaten_HLKS_Abgabe.objects.get(
-            gewerk=self.gewerk, raumnutzung=self.raumnutzung, abgabesystem=self.abgabesystem, gebaudenutzung=self.gebaudenutzung)
-        pro_m3h = kostenstammdaten_HLKS_Abgabe.einheitspreis_pro_m2
-        return pro_m3h
-
-    def getKaeltKostenAbgabe(self):
-        kostenstammdaten_HLKS_Abgabe: Kostenstammdaten_HLKS_Abgabe = Kostenstammdaten_HLKS_Abgabe.objects.get(
-            gewerk=self.gewerk, raumnutzung=self.raumnutzung, abgabesystem=self.abgabesystem, gebaudenutzung=self.gebaudenutzung)
-        pro_kw = kostenstammdaten_HLKS_Abgabe.einheitspreis_pro_m2
-        return pro_kw
+from helios.projekt.models import Abgabesystem, Erzeugungstyp, Gewerk, Gewerk2, Klassifizierung, Projekt, Stammdaten_Technickzentralen_Elektro, Technikzentralstammdaten_HLKS
 
 
 class Technikflaechen(models.Model):
     projekt = ForeignKey(Projekt, on_delete=SET_NULL, null=True)
-    stammdaten_technikzentrale_elektro = FloatField()
-    stammdaten_technikzentrale_hlks = FloatField()
+    stammdaten_technikzentrale_elektro = ForeignKey(Stammdaten_Technickzentralen_Elektro, on_delete=SET_NULL, null=True)
+    stammdaten_technikzentrale_hlks = ForeignKey(Technikzentralstammdaten_HLKS, on_delete=SET_NULL, null=True)
     leistung_pro_gewerk = FloatField()
     luftwechsel = FloatField()
     gewerk = ForeignKey(Gewerk, on_delete=SET_NULL, null=True)
@@ -86,13 +34,38 @@ class Leistung(models.Model):
     luftwechsel_pro_Person_Klassifizierung = FloatField(blank=True, null=True)
     flaeche_pro_Personenanzahl_Klassifizierung = FloatField(blank=True, null=True)
     raumtemparatur_Klassifizierung = FloatField(blank=True, null=True)
-    #Berechnete Werte
+    # Berechnete Werte
     leistung_pro_gewerk = FloatField(blank=True, null=True)
     personenanzahl_pro_nutzung = FloatField(blank=True, null=True)
     luftwechsel_pro_nutzung = FloatField(blank=True, null=True)
-    
 
     def __str__(self):
         return str(self.leistung_pro_m2_Klassifizierung_Gewerk2)
 
 
+class Investitionskosten(models.Model):
+    projekt = ForeignKey(Projekt, on_delete=CASCADE, null=True, blank=True)
+    flaeche = IntegerField()
+    leistung = ForeignKey(Leistung, on_delete=SET_NULL, null=True)
+    gewerk = ForeignKey(Gewerk, on_delete=SET_NULL, null=True)
+    abgabesystem = ForeignKey(Abgabesystem, on_delete=SET_NULL, null=True)
+    einheitspreis_pro_m2 = IntegerField()
+    einheitspreis_pro_kw = IntegerField()
+    erzeugungstyp = ForeignKey(Erzeugungstyp, on_delete=SET_NULL, null=True)
+
+    def __str__(self):
+        return self.pk or ''
+
+class Energie(models.Model):
+    projekt = ForeignKey(Projekt, on_delete=CASCADE, null=True, blank=True)
+    klassifizierung = ForeignKey(Klassifizierung,on_delete=SET_NULL,null=True)
+    gewerk=ForeignKey(Gewerk,on_delete=SET_NULL,null=True)
+
+    def __str__(self):
+        return self.pk
+    
+class Nutzungskosten(models.Model):
+    projekt = ForeignKey(Projekt,on_delete=SET_NULL)
+
+    def __str__(self):
+        return self.pk
